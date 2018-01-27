81b0a05ddd4f805bf6f01ffe454372c6e4068d73
nandoascal <fxa6511@rit.edu>
2018-01-27 17:02:56 -0500

    allows modification of AWS values

---
+++ AWSWrapper/modify.py
@@ -0,0 +1,11 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+
+def mondify(instance, attribute, value):
+    ec2.stop_instances(InstancesIds =[instance])
+    waiter = ec2.get_waiter('instance_stopped')
+    waiter.wait(InstanceIds=[instance])
+
+    ec2.modify_instance_attribute(InstanceId=instance, Attribute=attribute, Value=value)
