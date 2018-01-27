080dce9d307c927968a29fb54ea865ad05f1c6fe
Devin Matte <devinmatte@gmail.com>
2018-01-27 11:28:40 -0500

    Basic Setup

--- AWSWrapper/__init__.py
+++ AWSWrapper/__init__.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://brickhack:' + password + '@' + ip + '/brickhack'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://brickhack:' + password + '@' + ip + '/brickhack'
 db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["GET"])
+def home(auth_dict=None):
+    return render_template("index.html")
