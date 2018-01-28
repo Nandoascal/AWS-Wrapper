653623feb23dd8fc9adc16d3d2a220b781da1f51
Devin Matte <devinmatte@gmail.com>
2018-01-27 21:08:26 -0500

    Fixing Jacks Code again

--- AWSWrapper/Instances.py
+++ AWSWrapper/Instances.py
@@ -1,6 +1,5 @@
 import boto3
 
-
 ec2_resource = boto3.resource('ec2')
 ec2_client = boto3.client('ec2')
 
@@ -10,34 +9,33 @@ class Instances:
     def __init__(self):
         self.instance_list = []
 
-
     def get_all_info(self):
         instances_info = {}
 
         for instance in self.instance_list:
             instances_info[instance.instance_id] = instance.get_info()
+        return instances_info
 
     ### Add key pair functionality to both ###
     def launch_named_instance(self, name, image_id):
         instance = ec2_resource.create_instances(
-                ImageId=image_id,
-                MaxCount=1,
-                MinCount=1,
-                TagSpecifications=[
-                    {
-                        'ResourceType':'instance',
-                        'Tags':[
-                            'Key':'Name',
-                            'Value':name
-                            ]
-                        }
-                    ]
-                )
-
+            ImageId=image_id,
+            MaxCount=1,
+            MinCount=1,
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': {
+                        'Key': 'Name',
+                        'Value': name
+                    }
+                }
+            ]
+        )
 
     def launch_instance(self, image_id):
         instance = ec2_resource.create_instances(
-                ImageId=image_id,
-                MaxCount=1,
-                MinCount=1
-                )
+            ImageId=image_id,
+            MaxCount=1,
+            MinCount=1
+        )
